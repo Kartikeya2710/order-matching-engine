e83a9ce93747d11d46714d83c0927d53cb5c4462
Kartikeya Sharma <kartikeya.rahul.sharma@gmail.com>
2025-12-16 23:04:33 +0530

    Starter project with CMake

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  order-matching-engine
+//
+//  Created by Kartikeya Sharma on 14/12/25.
+//
+
+#include <iostream>
+
+int main() {
+    // insert code here...
+    std::cout << "Hello from order matching engine!!" << std::endl;
+    return EXIT_SUCCESS;
+}
