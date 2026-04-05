9e38ddecfc72a3e51547f4ce1b3a8b8e5ada25bc
Kartikeya Sharma <kartikeya.rahul.sharma@gmail.com>
2026-04-05 17:09:23 +0530

    Included cstdint

--- include/Types.hpp
+++ include/Types.hpp
@@ -1,27 +1,28 @@
 #pragma once
+#include <cstdint>
 
 namespace engine::types
 {
-    using OrderId = std::uint32_t;
-    using ClientId = std::uint32_t;
-    using Quantity = std::uint32_t;
-    using Price = std::uint32_t;
-    using InstrumentId = std::uint32_t;
+    using OrderId = uint32_t;
+    using ClientId = uint32_t;
+    using Quantity = uint32_t;
+    using Price = uint32_t;
+    using InstrumentId = uint32_t;
 
-    enum class Verb : std::uint16_t
+    enum class Verb : uint16_t
     {
         Buy = 0,
         Sell = 1,
     };
 
-    enum class OrderType : std::uint16_t
+    enum class OrderType : uint16_t
     {
         Market = 0,
         Limit = 1,
         Stop = 2
     };
 
-    enum class TimeInForce : std::uint16_t
+    enum class TimeInForce : uint16_t
     {
         FOK = 0,
         IOC = 1,
